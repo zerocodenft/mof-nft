0951d02ece33ded1628057a8a257d65d9f9aa74d
mykhailo <m.diakovych13@gmail.com>
2022-11-25 17:21:20 +0200

    Added Mblur component, fixed background for team cards

--- components/home/roadmap/HomeRoadmap.vue
+++ components/home/roadmap/HomeRoadmap.vue
@@ -84,46 +84,23 @@ export default {
 	transition: all .4s cubic-bezier(1.0, 0.5, 0.8, 1.0);
 }
 
-.slide-fade-enter, .slide-fade-leave-to
-	/* .slide-fade-leave-active below version 2.1.8 */
-{
+.slide-fade-enter, .slide-fade-leave-to {
 	transform: translateX(10px);
 	opacity: 0;
 }
-
-$delay: 100ms;
-$delayStep: 100ms;
-
-.fade-enter,
-.fade-leave-to {
-	& > * {
-		opacity: 0;
-		transform: translateY(40px);
-	}
+.list-move,
+.list-enter-active,
+.list-leave-active {
+	transition: all 0.5s ease;
 }
 
-.fade-enter-active {
-	& > * {
-		&:nth-child(2) {
-			transition-delay: 100ms;
-		}
-
-		&:nth-child(3) {
-			transition-delay: 200ms;
-		}
-	}
+.list-enter-from,
+.list-leave-to {
+	opacity: 0;
+	transform: translateX(30px);
 }
-$delay: 100ms;
-$delayStep: 100ms;
-.fade-leave-active {
-	& > * {
-		&:nth-child(1) {
-			transition-delay: 200ms;
-		}
 
-		&:nth-child(2) {
-			transition-delay: 100ms;
-		}
-	}
+.list-leave-active {
+	position: absolute;
 }
 </style>
