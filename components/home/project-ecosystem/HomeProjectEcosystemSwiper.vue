5690e193831fdbf562805233e99bb7433862dd73
mykhailo <m.diakovych13@gmail.com>
2022-12-15 09:55:55 +0200

    Changed to gradient icons in ecosystem project component

--- components/home/project-ecosystem/HomeProjectEcosystemSwiper.vue
+++ components/home/project-ecosystem/HomeProjectEcosystemSwiper.vue
@@ -23,8 +23,9 @@
             <template #default>
               <div class="project-ecosystem__content d-flex flex-column align-items-center">
                 <b-img
-                  class="d-block project-ecosystem__logo"
-                  :src="require(`assets/img/project-ecosystem/${item.logo}.svg`)"
+                  class="d-block"
+                  :class="[`project-ecosystem__logo${index+1}`]"
+                  :src="require(`assets/img/project-ecosystem/${item.logo}`)"
                   alt="ball"
                 />
                 <p class="project-ecosystem__title">{{ item.title }}</p>
@@ -75,11 +76,11 @@ export default {
       spaceBetween: 30
     }
     const projects = ref([
-      {id: 1, title: 'offline (irl) museum of footballs',subTitle:"IRL football events", description: "The concept of footballs museum in unique. Since 2013, we have accrued an unprecedented collection of football from history's most significant football tournaments, spanning over 1000 balls. More than that, even historical rarity balls since Middle Age era are part of this collection, which has big historical value for the international football community", logo: 'museum'},
-      {id: 2, title: 'metaverse vmuseum of footballs', description: 'vMuseum of Footballs is the official source of information on the history of each exhibit and all collection. Also this an additional tool that draws the attention of collectors around the world to our project', logo: 'web'},
-      {id: 3, title: 'vfootballs nft collection', subTitle:"launched on Ethereum blockchain", description: "The unique NFT collection will become the sport's most prized Web3 trophy, which will be in the heart of every football fan-a must-have for future generations of football fans worldwide. Owning a piece of footballing history would surely bring back emotions from a tournament and keep them alive forever, but it will also have its significant perks.", logo: 'nft'},
-				 	{id: 4, title: "vgoal play-to-earn game", subTitle: '', description: "vGoal game, as the first blockchain-based football metaverse,gives its user access to manage their club and generate income while playing.In this game, every player or user can actively take part in different methods and earn rewards for their contributions to the ecosystem.", logo: "goggles"},
-      {id:5, title: "website catalog with ar technology", subTitle: "", description:"The museum exists off-line, on-line, and as the world's first museum in the Metaverse. Each ball from our collection can not only be viewed, but also touched in 3D and placed anywhere you wish in augmented reality", logo: "controller"}
+      {id: 1, title: 'offline (irl) museum of footballs',subTitle:"IRL football events", description: "The concept of footballs museum in unique. Since 2013, we have accrued an unprecedented collection of football from history's most significant football tournaments, spanning over 1000 balls. More than that, even historical rarity balls since Middle Age era are part of this collection, which has big historical value for the international football community", logo: 'museum.png'},
+      {id: 2, title: 'metaverse vmuseum of footballs', description: 'vMuseum of Footballs is the official source of information on the history of each exhibit and all collection. Also this an additional tool that draws the attention of collectors around the world to our project', logo: 'web.png'},
+      {id: 3, title: 'vfootballs nft collection', subTitle:"launched on Ethereum blockchain", description: "The unique NFT collection will become the sport's most prized Web3 trophy, which will be in the heart of every football fan-a must-have for future generations of football fans worldwide. Owning a piece of footballing history would surely bring back emotions from a tournament and keep them alive forever, but it will also have its significant perks.", logo: 'nft1.png'},
+				 	{id: 4, title: "vgoal play-to-earn game", subTitle: '', description: "vGoal game, as the first blockchain-based football metaverse,gives its user access to manage their club and generate income while playing.In this game, every player or user can actively take part in different methods and earn rewards for their contributions to the ecosystem.", logo: "goggles.png"},
+      {id:5, title: "website catalog with ar technology", subTitle: "", description:"The museum exists off-line, on-line, and as the world's first museum in the Metaverse. Each ball from our collection can not only be viewed, but also touched in 3D and placed anywhere you wish in augmented reality", logo: "game.png"}
     ])
 
     const prevSlide = () => {
